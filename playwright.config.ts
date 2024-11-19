a7363a6184d52b1b38ba2cf55476e2c87bafa785
Gal Ovadia <gal.ovadia@gatech.edu>
2024-11-19 17:57:20 -0500

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,9 +71,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run dev", // Adjust as needed
+    url: "http://localhost:3000",
+    timeout: 120 * 1000,
+    reuseExistingServer: false, // Ensure a fresh server is started in CI
+  },
 });
