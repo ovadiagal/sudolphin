70c1256f7b3fb906ba702283ed681d70cf84dfd2
mfrank40 <mfrank40@gatech.edu>
2024-11-26 13:44:03 -0500

    super important work

--- tests/playwright/user.test.ts
+++ tests/playwright/user.test.ts
@@ -14,7 +14,6 @@ test.describe("Authentication Flow", () => {
     await page.fill('input[name="email"]', "user@example.com");
     await page.fill('input[name="password"]', "password");
     await page.click('button[type="submit"]');
-
     // Check that the user is redirected to the dashboard
     await expect(page).toHaveURL("/protected");
   });
@@ -38,14 +37,14 @@ test.describe.serial("Dashboard Interactions", () => {
     await page.fill('input[name="password"]', "password");
     await page.click('button[type="submit"]');
     await Promise.all([
-      page.waitForURL('/protected'),  // Wait for redirect to protected route
+      // Wait for redirect to protected route
+      page.waitForURL('/protected'),
     ]);
   });
 
   test("add new course (default color)", async ({ page }) => {
     await page.goto("/protected");
     await page.locator('div').filter({ hasText: /^Add Class$/ }).nth(2).click();
-
     // Fill in class name
     const newClassName = "Test Course One";
     await page.fill('input[name="className"]', newClassName);
@@ -63,7 +62,6 @@ test.describe.serial("Dashboard Interactions", () => {
     await page.getByRole("button", { name: "Create Class" }).click();
     // Check that the course is visible on the dashboard
     await expect(page.getByText("Test Course Two")).toBeVisible();
-
     // Find the course div by text content and verify its color
     const courseDiv = await page.locator("div.rounded-lg", {
       has: page.getByText("Test Course Two"),
@@ -83,16 +81,13 @@ test.describe.serial("Dashboard Interactions", () => {
   test("delete course from dashboard", async ({ page }) => {
     await page.goto("/protected");
     const courseText = "Test Course One";
-    // await page.locator('div').filter({ hasText: /^Test Course One$/ }).getByRole('button').click();
     await page.getByRole('button').nth(3).click();
-
     await expect(page.getByText(courseText)).not.toBeVisible();
   });
 
   test("delete course from dashboard (two)", async ({ page }) => {
     await page.goto("/protected");
     const courseText = "Test Course Two";
-    // await page.locator('div').filter({ hasText: /^Test Course Two$/ }).getByRole('button').click();
     await page.getByRole('button').nth(1).click();
 
     await expect(page.getByText(courseText)).not.toBeVisible();
@@ -101,7 +96,6 @@ test.describe.serial("Dashboard Interactions", () => {
   test("add new course for files (default color)", async ({ page }) => {
     await page.goto("/protected");
     await page.locator('div').filter({ hasText: /^Add Class$/ }).nth(2).click();
-
     // Fill in class name
     const newClassName = "File Upload Test Course";
     await page.fill('input[name="className"]', newClassName);
@@ -112,19 +106,16 @@ test.describe.serial("Dashboard Interactions", () => {
   test("successful file upload", async ({ page }) => {
     await page.goto("/protected");
     await page.getByRole('link', { name: 'File Upload Test Course' }).click();
-
     const fileChooserPromise = page.waitForEvent("filechooser");
     await page.locator('div').filter({ hasText: /^Drag and drop files here, or click to select$/ }).click();
     const fileChooser = await fileChooserPromise;
     await fileChooser.setFiles("./tests/playwright/shrek_textbasedPDF.pdf");
-
     await expect(page.getByText("Upload Complete!")).toBeVisible();
   });
 
   test("file upload (multiple files)", async ({ page }) => {
     await page.goto("/protected");
     await page.getByRole('link', { name: 'File Upload Test Course' }).click();
-
     const fileChooserPromise = page.waitForEvent("filechooser");
     await page.locator('div').filter({ hasText: /^Drag and drop files here, or click to select$/ }).click();
     const fileChooser = await fileChooserPromise;
@@ -133,7 +124,6 @@ test.describe.serial("Dashboard Interactions", () => {
       './tests/playwright/shrek2_textbasedPDF.pdf'
     ];
     await fileChooser.setFiles(filesToUpload);
-
     await expect(page.getByText("Upload Complete!")).toBeVisible();
   });
 
@@ -141,7 +131,6 @@ test.describe.serial("Dashboard Interactions", () => {
     await page.goto("/protected");
     const courseText = "File Upload Test Course";
     await page.getByRole('button').nth(1).click();
-
     await expect(page.getByText(courseText)).not.toBeVisible();
   });
 });
